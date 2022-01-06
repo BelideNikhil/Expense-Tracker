e45a9e71d49f6bb715d162b0a857d2855a8bc44b
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2022-01-06 17:36:02 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
-
-function App() {
+import {Route,BrowserRouter,Routes} from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
+//
+import Navbar from './Components/Navbar';
+import Login from './Pages/Login'
+import Logout from './Pages/Logout'
+import SignUp from './Pages/SignUp'
+import Dashboard from './Pages/Dashboard'
+//
+import PrivateRoute from './Components/PrivateRoute';
+//
+import { useAuth } from './Hooks/useAuth';
+export default function App() {
+  const {user,authIsReady}=useAuth()
   return (
     <div className="App">
-      Hello
+      {authIsReady&&
+      <>
+        <BrowserRouter>
+        <Navbar/>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={user?<Dashboard/>:<Navigate to="/login" />}/>
+            <Route path="/signup"  element={user?<Navigate to="/" />:<SignUp/>}></Route>
+            <Route path="/login" element={user?<Navigate to="/" />:<Login/>}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+      </>}
     </div>
   );
 }
-
-export default App;
